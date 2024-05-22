5f1ef6294d1f4d596cda7159f453a029bcd9366c
ansarihalima <halimasadiahalimasadia00@gmail.com>
2024-05-22 03:34:18 -0700

    chapter-1

---
+++ index.js
@@ -0,0 +1,36 @@
+/////////////////Question:1
+
+// Script to greet website visitors using JS alert box
+alert("Welcome to our website!🎉 We're glad you're here.");
+
+
+/////////////////Question:2
+
+///Write a script to display following message on your web page:
+alert("Error! Please enter a valid password");
+
+/////////////////Question:3
+
+// Script to display a message with line breaks on the web page
+alert("Welcome to our page.  \n    Happy coding!😃");
+
+
+/////////////////Question:4
+
+// Script to display messages in sequence using alert box
+alert("Welcome to Js Land...");
+// Script to greet the user
+alert("Happpppy coding!");
+
+/////////////////Question:5
+
+
+////  alert("Hello ! I can run JS through my web browser console")
+console.log("Hello ! I can run JS through my web browser console");
+
+
+alert("Assignment-6")
+alert("PH-Scale")
+
+
+
